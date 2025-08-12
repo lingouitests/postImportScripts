76f7cfb07b5fec7ccd4b16e037047c61856ee7c0
lingouitests <lingouitests@gmail.com>
2025-08-12 16:18:34 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "============================================================"
+echo "   Post Import Script START "
+echo "============================================================"
+echo
+echo "LRM_GROUP=${LRM_GROUP}"
+echo "LRM_PROJECT=${LRM_PROJECT}"
+echo "KIT_VERSION=${KIT_VERSION}"
+echo "LRM_LOCALE=${LRM_LOCALE}"
+echo "IMPORT_STATUS=${IMPORT_STATUS}"
+
+if [ "${IMPORT_STATUS}" = "success" ]
+then
+echo "SUCCESS"
+else
+echo "FAILURE"
+fi
+
+echo
+echo "============================================================"
