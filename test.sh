b65615f1f6874a282ec9d77f2515b007198f4772
lingouitests <lingouitests@gmail.com>
2025-09-04 16:20:28 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,13 +9,7 @@ echo "LRM_PROJECT=${LRM_PROJECT}"
 echo "KIT_VERSION=${KIT_VERSION}"
 echo "LRM_LOCALE=${LRM_LOCALE}"
 echo "IMPORT_STATUS=${IMPORT_STATUS}"
-
-if [ "${IMPORT_STATUS}" = "success" ]
-then
-echo "SUCCESS POSTIMPORT SCRIPT edit for new hash"
-else
-echo "FAILURE POSTIMPORT SCRIPT"
-fi
-
-echo THIS IS A POST IMPORT SCRIPT
+echo
+echo "============================================================"
+echo " THIS IS A POST IMPORT SCRIPT "
 echo "============================================================"
